c5a27a10d5d71f220bd63061e14cf35ff59cf784
Mansi1108 <mansillamelissa@gmail.com>
2021-10-26 17:11:01 -0600

    Person Register Arreglado

--- sql_querys/procedure_personregister.sql
+++ sql_querys/procedure_personregister.sql
@@ -2,8 +2,21 @@
 USE DBBoatAdministration
 GO
 --Procedimiento de ingreso de N clientes
+create or alter procedure sp_RegisterPerson @method int
+as
+begin
+	if(@method = 0)
+	begin
+		exec sp_RegisterPersonM0
+	end
+	else
+	begin
+		exec sp_RegisterPersonM1
+	end
+end;
 
-create or alter procedure sp_Register @method int
+--Procedimiento de metodo 0
+create or alter procedure sp_RegisterPersonM0
 as
 begin
 	create table #tempPersonR
@@ -16,19 +29,16 @@ begin
 		lastnamePerson varchar(150),
 		dateOfBirth date
 	)
-	BULK INSERT #tempPersonR
-	FROM 'C:\Users\Owner-1\Documents\BASE DE DATOS 2\informacion.csv'
-	WITH
-	(
-		FIELDTERMINATOR = ',',  --CSV field delimiter
-		ROWTERMINATOR = '\n',   --Use to shift the control to next row
-		TABLOCK
-	)
-	--Revisa si usara el metodo de rollback masivo, si este lo es inicia la transaccion antes del cursor, si no no lo abre.
-	if(@method = 0)
-	begin
-	BEGIN TRANSACTION
-	end
+
+	--BULK INSERT #tempPersonR
+	--FROM 'filepath'
+	--WITH
+	--(
+	--	FIELDTERMINATOR = ',',  --CSV field delimiter
+	--	ROWTERMINATOR = '\n',   --Use to shift the control to next row
+	--	TABLOCK
+	--)
+
 	Declare	
 		 @idPersonType int, 
 		 @idBloodType int, 
@@ -38,52 +48,99 @@ begin
 		 @lastnamePerson varchar(150), 
 		 @dateOfBirth date 
 
+	-- Se inicia afuera para hacer un rollback masivo, y manejarlo como uno solo.
+	BEGIN TRANSACTION
+	-- el cursor se basa en una tabla temporal con los archivos recien añadidos.
 	DECLARE CursorPerR CURSOR FOR select * from #tempPersonR
 	OPEN  CursorPerR
 	FETCH NEXT FROM  CursorPerR into @idPersonType, @idBloodType, @idCountry, @identificationNumber, @namePerson, @lastnamePerson, @dateOfBirth
 	WHILE @@FETCH_STATUS = 0
 	begin 
-		BEGIN TRY 
-			-- Si es el otro metodo de continuar sin confirmar, inicia la transaccion de manera individual.
-			if(@method = 1)
-			begin
-				BEGIN TRANSACTION
-				if exists(Select * from Person where identificationNumber = @identificationNumber)
-				begin
-				--Continua sin confirmar, simplemente haciendo rollback a la persona repetida.
-					rollback transaction
-					print ('Esta repetido la Persona')
-				end
-				ELSE
-				begin
-					exec sp_AddPerson @idPersonType = @idPersonType, @idBloodType = @idBloodType, @idCountry = @idCountry, @identificationNumber =@identificationNumber, @namePerson = @namePerson, 
-					@lastnamePerson = @lastnamePerson, @dateOfBirth = @dateOfBirth
-					commit;
-				end
-			end	
-			ELSE IF(@method = 0)
-			BEGIN
-				if exists(Select * from Person where identificationNumber = @identificationNumber)
-				begin
-				--Realiza el rollback sobre todas las personas insertadas.
-					rollback transaction
-					print ('Esta repetido la Persona')
-				end
-				ELSE
-				begin
-					exec sp_AddPerson @idPersonType = @idPersonType, @idBloodType = @idBloodType, @idCountry = @idCountry, @identificationNumber =@identificationNumber, @namePerson = @namePerson, 
-					@lastnamePerson = @lastnamePerson, @dateOfBirth = @dateOfBirth
-					commit;
-				end
-			END
-			FETCH NEXT FROM  CursorPerR into @idPersonType, @idBloodType, @idCountry, @identificationNumber, @namePerson, @lastnamePerson, @dateOfBirth
-		END TRY
-		begin catch 
-			print 'Método no Válido'
-		end catch
+
+		if exists(Select * from Person where identificationNumber = @identificationNumber)
+		begin
+		--Realiza rollback a todos los datos anteriormente ingresados, e inicia una nueva transaccion.
+			rollback transaction
+			print ('Esta repetido la Persona')
+			BEGIN TRANSACTION
+		end
+		ELSE
+		begin
+			exec sp_AddPerson @idPersonType = @idPersonType, @idBloodType = @idBloodType, @idCountry = @idCountry, @identificationNumber =@identificationNumber, @namePerson = @namePerson, 
+			@lastnamePerson = @lastnamePerson, @dateOfBirth = @dateOfBirth
+		end
+
+		FETCH NEXT FROM  CursorPerR into @idPersonType, @idBloodType, @idCountry, @identificationNumber, @namePerson, @lastnamePerson, @dateOfBirth
+	end		
+	close CursorPerR
+	Deallocate  CursorPerR
+	-- Si no hubo nadie repetido, se realizara el commit de todas las personas ya ingresadas, de lo contrario no añadira nada.
+	commit transaction
+	Drop table #tempPersonR
+END;
+
+--Procedimiento de metodo 1
+create or alter procedure sp_RegisterPersonM1
+as
+begin
+	create table #tempPersonR2
+	(
+		idPersonType int,
+		idBloodType int, 
+		idCountry int,
+		identificationNumber varchar(25),
+		namePerson varchar(150),
+		lastnamePerson varchar(150),
+		dateOfBirth date
+	)
+
+		--BULK INSERT #tempPersonR
+	--FROM 'filepath'
+	--WITH
+	--(
+	--	FIELDTERMINATOR = ',',  --CSV field delimiter
+	--	ROWTERMINATOR = '\n',   --Use to shift the control to next row
+	--	TABLOCK
+	--)
+
+	Declare	
+		 @idPersonType int, 
+		 @idBloodType int, 
+		 @idCountry int, 
+		 @identificationNumber varchar(25),
+		 @namePerson varchar(150), 
+		 @lastnamePerson varchar(150), 
+		 @dateOfBirth date 
+	-- el cursor se basa en una tabla temporal con los archivos recien añadidos.
+	DECLARE CursorPerR CURSOR FOR select * from #tempPersonR2
+	OPEN  CursorPerR
+	FETCH NEXT FROM  CursorPerR into @idPersonType, @idBloodType, @idCountry, @identificationNumber, @namePerson, @lastnamePerson, @dateOfBirth
+	WHILE @@FETCH_STATUS = 0
+	begin 
+		--Inicia transaccion individual
+		BEGIN TRANSACTION
+		if exists(Select * from Person where identificationNumber = @identificationNumber)
+		begin
+		--Si esta repetida realiza rollback solamente a ese registro
+			rollback transaction
+			print ('Esta repetido la Persona')
+		end
+		ELSE
+		begin
+		-- al ser individual hace commit por cada registro
+			exec sp_AddPerson @idPersonType = @idPersonType, @idBloodType = @idBloodType, @idCountry = @idCountry, @identificationNumber =@identificationNumber, @namePerson = @namePerson, 
+			@lastnamePerson = @lastnamePerson, @dateOfBirth = @dateOfBirth
+			commit transaction
+		end
+		FETCH NEXT FROM  CursorPerR into @idPersonType, @idBloodType, @idCountry, @identificationNumber, @namePerson, @lastnamePerson, @dateOfBirth
+
 	end		
 	close CursorPerR
 
 	Deallocate  CursorPerR
-	Drop table #tempPersonR
+	Drop table #tempPersonR2
 END;
+
+
+
+
