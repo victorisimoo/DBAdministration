294cf0a46f2de81e404481c1f6ce1b13d3cda21c
JohnSMH <55027808+JohnSMH@users.noreply.github.com>
2021-10-26 22:26:44 -0600

    Test added

--- sql_querys/Trigger_PortManagement.sql
+++ sql_querys/Trigger_PortManagement.sql
@@ -38,3 +38,26 @@ begin
 	SELECT idTravelRoute,descriptionDebarking,dayDebarking,hourDebarking from inserted
 end
 end;
+
+--Test de funcionamiento
+--Generar un nuevo route
+--exec dbo.sp_AddTravelRoute @nauticalMiles = 2.00, @hoursOfTravel = 2.00, @descriptionRoute = 'hola xd'
+
+--Generar un nuevo travel
+--exec dbo.sp_AddTravel @idDeparture = 1, @idDestination =1, @idBoat = 41, @idTravelRoute = 102, @idPerson = 1, @startDate = '2021-07-07 18:06:46.000', @endDate = '2021-07-07 18:06:46.000', @descriptionTravel = 'holaxd'
+--Generar Debarkings
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 1', @dayDebarking = '2021-10-26',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd error', @dayDebarking = '2021-10-26',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 2', @dayDebarking = '2021-10-27',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 3', @dayDebarking = '2021-10-28',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 4', @dayDebarking = '2021-10-29',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 5', @dayDebarking = '2021-10-30',@hourDebarking = '4:12:35.000'
+--exec dbo.sp_AddDebarking @idTravelRoute = 102,@descriptionDebarking = 'hola xd 6 error', @dayDebarking = '2021-10-31',@hourDebarking = '4:12:35.000'
+
+--Para visualizar
+--SELECT * FROM Travel
+--SELECT * FROM TravelRoute
+--SELECT * FROM Debarking
+--DELETE FROM Travel WHERE idTravel = 53 --por definir
+--DELETE FROM TravelRoute WHERE idTravelRoute = --por definir
+--DELETE FROM Debarking WHERE idDebarking = por definir or idDebarking = por definir or idDebarking = por definir or idDebarking = por definir or idDebarking = por definir
