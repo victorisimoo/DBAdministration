69ffeba7ee64baf06ac3df2ab6c19bcfef2de495
JohnSMH <55027808+JohnSMH@users.noreply.github.com>
2021-10-26 21:41:47 -0600

    Comment

--- sql_querys/Trigger_PortManagement.sql
+++ sql_querys/Trigger_PortManagement.sql
@@ -9,18 +9,19 @@ declare
 @date date,
 @count int,
 @idtravel int;
-
+--Obtener fecha de insercion
 Select @date = dayDebarking, @idtravel=idTravelRoute from inserted;
-
+--Revisar si el dia ya esta en la tabla
 SELECT @occupied = COUNT(1)
 From Debarking
 WHERE dayDebarking = @date;
-
+--Revisar cuantos debarkings tiene un travel
 SELECT @count = COUNT(1) FROM Debarking
 WHERE idTravelRoute = @idtravel
-
+--Si se cumple alguno de los dos errores detener la insercion
 if(@occupied >=1 or @count>=5)
 begin
+--Verificar que error fue el que ocurrio
 	if(@occupied >=1)
 	begin
 		print('Error--El puerto ya esta ocupado por el dia')
@@ -32,6 +33,7 @@ begin
 end
 else 
 begin
+--Todo bien insertar
 	Insert into Debarking(idTravelRoute,descriptionDebarking,dayDebarking,hourDebarking)
 	SELECT idTravelRoute,descriptionDebarking,dayDebarking,hourDebarking from inserted
 end
